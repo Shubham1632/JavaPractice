5700b7556885644f2be4dcaeaae6faca3b834041
Shubham Gangurde <79448312+Shubham1632@users.noreply.github.com>
2023-01-19 18:10:16 +0530

    string

---
+++ src/string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(String[] args) {
+        String s = new String("Shubham");
+        System.out.println(s.charAt(0));
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.startsWith("Shu"));
+        System.out.println(s.replace('h','i'));
+    }
+}

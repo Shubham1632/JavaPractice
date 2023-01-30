4ccdfe522785e2ea8c2d311932e5ab634e134cd1
Shubham Gangurde <79448312+Shubham1632@users.noreply.github.com>
2023-01-30 22:30:02 +0530

    started with inheritance

--- src/Classes.java
+++ src/Classes.java
@@ -1,11 +1,13 @@
 
 class A{
-     int a;
-     int b;
+
+    protected int a;
+    protected int b;
     public A(int a,int b){
         this.a=a;
         this.b=b;
     }
+    public A(){}
     public int geta(){
         return a;
     }
@@ -17,12 +19,21 @@ class A{
     }
 }
 
+class B extends A{
+    public B(int a, int b){
+        this.a = a;
+        this.b= b;
+    }
+}
+
 public class Classes {
 
     public static void main(String[] args) {
 
         A obj = new A(5,6);
         System.out.println(obj.sum());
+        B obj1 = new B(9,7);
+        System.out.println(obj1.sum());
 
     }
 }

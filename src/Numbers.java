7cbd3c1968a3336834c4060bf748fcec2cd196a6
Shubham Gangurde <79448312+Shubham1632@users.noreply.github.com>
2023-01-31 21:44:13 +0530

    varargs used

--- src/Numbers.java
+++ src/Numbers.java
@@ -6,15 +6,19 @@ class num {
 }
 public class Numbers {
 
-    static int add(int a, int b){
-        return a+b;
+    static int add(int ...arr){
+        int res = 0;
+        for(int x : arr){
+            res += x;
+        }
+        return res;
     }
-    static void tellJoke(){
-        System.out.println("this is the joke");
-    }
-    static  void tellJoke(int a){
-        System.out.println("this is the joke with number");
-    }
+//    static void tellJoke(){
+//        System.out.println("this is the joke");
+//    }
+//    static  void tellJoke(int a){
+//        System.out.println("this is the joke with number");
+//    }
     public static void main(String[] args) {
 //        int a=20;
 //        Integer b = new Integer(20);
@@ -24,7 +28,10 @@ public class Numbers {
 //        System.out.println(add(a,b));
 //        System.out.println(a);
 //        System.out.println(b);
-        tellJoke();
-        tellJoke(3);
+//        tellJoke();
+//        tellJoke(3);
+        System.out.println(add(1));
+        System.out.println(add(2,5));
+        System.out.println(add(42,64,23,87));
     }
 }

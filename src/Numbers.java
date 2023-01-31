479aa75b0f41fb6d2fb4f381d8018a871646acdc
Shubham Gangurde <79448312+Shubham1632@users.noreply.github.com>
2023-01-31 21:27:16 +0530

    added static method

---
+++ src/Numbers.java
@@ -0,0 +1,22 @@
+class num {
+    num(){}
+    void change(int a){
+        a = 98;
+    }
+}
+public class Numbers {
+
+    static int add(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a=20;
+        Integer b = new Integer(20);
+        num obj = new num();
+        obj.change(a);
+        obj.change(b);
+        System.out.println(add(a,b));
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

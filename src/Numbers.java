2607bacdf168ccdfc97b69a644b98dbccb5e12f7
Shubham Gangurde <79448312+Shubham1632@users.noreply.github.com>
2023-01-31 21:30:18 +0530

    method overloading

--- src/Numbers.java
+++ src/Numbers.java
@@ -9,14 +9,22 @@ public class Numbers {
     static int add(int a, int b){
         return a+b;
     }
+    static void tellJoke(){
+        System.out.println("this is the joke");
+    }
+    static  void tellJoke(int a){
+        System.out.println("this is the joke with number");
+    }
     public static void main(String[] args) {
-        int a=20;
-        Integer b = new Integer(20);
-        num obj = new num();
-        obj.change(a);
-        obj.change(b);
-        System.out.println(add(a,b));
-        System.out.println(a);
-        System.out.println(b);
+//        int a=20;
+//        Integer b = new Integer(20);
+//        num obj = new num();
+//        obj.change(a);
+//        obj.change(b);
+//        System.out.println(add(a,b));
+//        System.out.println(a);
+//        System.out.println(b);
+        tellJoke();
+        tellJoke(3);
     }
 }
